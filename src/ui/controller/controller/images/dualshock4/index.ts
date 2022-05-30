aaa979e9cb7a867ea8ff6e7426cba24fa503a217
Martin Haynes <martin@dotmh.io>
2022-05-30 22:34:42 +0100

    Add right and left Analogue stick, refactor SVG names

--- src/ui/controller/controller/images/dualshock4/index.ts
+++ src/ui/controller/controller/images/dualshock4/index.ts
@@ -7,10 +7,9 @@ import {
   Home,
   Icon,
   Left,
-  LeftStick,
+  AnalogueStick,
   Option,
   Right,
-  RightStick,
   Share,
   Square,
   Triangle,
@@ -25,8 +24,8 @@ dualshock4ImageMap.set(Buttons.B, X);
 dualshock4ImageMap.set(Buttons.X, Triangle);
 dualshock4ImageMap.set(Buttons.Y, Square);
 
-dualshock4ImageMap.set(Triggers.L3, LeftStick);
-dualshock4ImageMap.set(Triggers.R3, RightStick);
+dualshock4ImageMap.set(Triggers.L3, AnalogueStick);
+dualshock4ImageMap.set(Triggers.R3, AnalogueStick);
 
 dualshock4ImageMap.set(Controls.SELECT, Option);
 dualshock4ImageMap.set(Controls.START, Share);
